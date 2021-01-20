75a26fb3f3277e7e2e3a2e9094a3e7a20c0eaaae
Luke Rep-Baihelfer <lukerep97@gmail.com>
2021-01-20 13:37:36 +1100

    Setup ESLint

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const morgan = require('morgan');
 
 const app = express();
 app.use(helmet());
-app.use(morgan("common"));
+app.use(morgan('common'));
 
 app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+	return res.send('Received a GET HTTP method');
 });
 
 app.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+	return res.send('Received a POST HTTP method');
 });
 
 app.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
+	return res.send('Received a PUT HTTP method');
 });
 app.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+	return res.send('Received a DELETE HTTP method');
 });
 
 app.listen(process.env.PORT || 3000, () =>
-  console.log(`Example app listening on port ${process.env.PORT || 3000}!`),
+	console.log(`Example app listening on port ${process.env.PORT || 3000}!`),
 );
