ee8aa93b857d936fee87e8eca2ad50f05f929681
Luke Rep-Baihelfer <lukerep97@gmail.com>
2021-01-21 10:19:00 +1100

    Started using lowdb

--- dataHandler/dataHandler.js
+++ dataHandler/dataHandler.js
@@ -1,21 +1,31 @@
-const contracts = [
-	{
-		header: 'Header line',
-		lines: [
-			{text: 'Line 1', value: 10},
-			{text: 'Line 2', value: 20},
-		]
-	}
-];
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const { v4: uuidv4 } = require('uuid');
+
+const adapter = new FileSync('data/data.json');
+const db = low(adapter);
+
+// Sets up database with some entry/s if the database file does not exist yet
+db.defaults({
+	contracts: [
+		{
+			id: uuidv4(),
+			state: 'DRAFT', // using a string to determine if DRAFT or ACTIVE instead of bool, incase we want additional states in the future
+			header: {
+				name: 'Luke',
+				address: 'Melbourne'
+			},
+			lines: [
+				{ text: 'Line 1', value: 1000 },
+				{ text: 'Line 2', value: 2000 },
+			]
+		}
+	]
+}).write();
 
 const getContract = (req, res) => {
 	const id = req.params.id;
-
-	if (isNaN(id) || id >= contracts.length) {
-		return res.send(404); //404 not found
-	}
-
-	return res.send(contracts[id]);
+	return res.send(db.get('contracts').find({ header: {name: id} }).value() || 404);
 };
 
 const putContract = (req, res) => {
